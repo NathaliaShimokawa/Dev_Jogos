c56abec181f3c366fe5f679187b7985f370bb281
NathaliaShimokawa <122635306+NathaliaShimokawa@users.noreply.github.com>
2025-03-25 19:44:21 -0300

    Lab SpaceShip completo

--- Space_Ship/Assets/Scripts/PowerupBehaviour.cs
+++ Space_Ship/Assets/Scripts/PowerupBehaviour.cs
@@ -2,29 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PowerupBehaviour : MonoBehaviour {
-
+public class PowerupBehaviour : MonoBehaviour
+{
     private float speed = 1f;
-    
+
     [SerializeField]
     private bool isSpeedPowerup = true; // Define que o power-up será de Speed
 
-    void Start() {
+    void Start()
+    {
         // Nenhum código necessário no Start, já que tudo ocorre no Update ou no OnTriggerEnter2D
     }
 
-    void Update() {
-        // O power-up agora se move na direção horizontal (para a direita ou para a esquerda)
+    void Update()
+    {
         transform.Translate(Vector3.left * speed * Time.deltaTime);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision) {
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
         // Quando o power-up colide com o jogador
-        if (collision.CompareTag("Player")) {
-
+        if (collision.CompareTag("Player"))
+        {
             // Verifica se o power-up é do tipo Speed
-            if (isSpeedPowerup) {
+            if (isSpeedPowerup)
+            {
                 collision.GetComponent<PlayerBehaviour>().SpeedPowerOn();
+
+                // Desacelera todos os inimigos na cena
+                EnemyBehaviour[] enemies = FindObjectsOfType<EnemyBehaviour>();
+                foreach (var enemy in enemies)
+                {
+                    enemy.SlowDownEnemy();
+                }
             }
 
             // Após ativar o power-up, destrua o objeto
