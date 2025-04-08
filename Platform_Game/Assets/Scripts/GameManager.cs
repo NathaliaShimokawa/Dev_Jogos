31babb2588ba6b06945a867fab0193221e989783
NathaliaShimokawa <122635306+NathaliaShimokawa@users.noreply.github.com>
2025-04-08 08:16:41 -0300

    Star_Game

---
+++ Platform_Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,163 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[DefaultExecutionOrder(-1)]
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    [SerializeField] private GameObject gameOverUI;
+    [SerializeField] private Text scoreText;
+
+    private PlayerBehaviour player;
+    private EnemyBehaviour[] enemies;
+
+    public int score { get; private set; } = 0;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            DestroyImmediate(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (Instance == this)
+        {
+            Instance = null;
+        }
+    }
+
+    private void Start()
+    {
+        player = Object.FindFirstObjectByType<PlayerBehaviour>();
+        enemies = Object.FindObjectsByType<EnemyBehaviour>(FindObjectsSortMode.None);
+
+        if (player == null)
+        {
+            Debug.LogError("PlayerBehaviour não encontrado na cena!");
+        }
+
+        NewGame();
+    }
+
+    private void Update()
+    {
+        // Se o score atingir 20, chama a função de vitória
+        if (score >= 10)
+        {
+            OnGameWon();
+        }
+    }
+
+    private void NewGame()
+    {
+        gameOverUI.SetActive(false);
+        SetScore(0);
+        NewRound();
+    }
+
+    private void NewRound()
+    {
+        foreach (EnemyBehaviour enemy in enemies)
+        {
+            enemy.ResetEnemy();
+            enemy.gameObject.SetActive(true);
+        }
+
+        Respawn();
+    }
+
+    private void Respawn()
+    {
+        if (player != null)
+        {
+            Vector3 position = player.transform.position;
+            position.x = 0f;
+            player.transform.position = position;
+            player.gameObject.SetActive(true);
+        }
+    }
+
+    private void GameOver()
+    {
+        // Desativa todos os inimigos quando o jogo termina
+        foreach (EnemyBehaviour enemy in enemies)
+        {
+            enemy.gameObject.SetActive(false);
+        }
+
+        // Carrega a cena de "Lose_Game"
+        SceneManager.LoadScene("Lose_Game");
+    }
+
+    private void SetScore(int score)
+    {
+        if(this.score == 10){
+            OnGameWon();
+        }
+
+        this.score = score;
+        scoreText.text = score.ToString().PadLeft(4, '0');
+    }
+
+    public void OnPlayerKilled(PlayerBehaviour player)
+    {
+        player.gameObject.SetActive(false);
+        GameOver();  // Se o jogador for destruído, o jogo termina
+    }
+
+    public void OnEnemyKilled(EnemyBehaviour enemy)
+    {
+        enemy.gameObject.SetActive(false);
+        SetScore(score + 1);  // Aumenta o score
+
+        // Verifica se todos os inimigos foram mortos
+        bool allEnemiesKilled = true;
+        foreach (EnemyBehaviour e in enemies)
+        {
+            if (e.gameObject.activeSelf)
+            {
+                allEnemiesKilled = false;
+                break;
+            }
+        }
+
+        // Inicia uma nova rodada se todos os inimigos foram mortos
+        if (allEnemiesKilled)
+        {
+            NewRound();
+        }
+    }
+
+    public void OnEnemyCollidedWithPlayer()
+    {
+        // Chama a função de Game Over se um inimigo colidir com o jogador
+        GameOver();
+    }
+
+    public void OnGameWon()
+    {
+        // Carrega a cena de "Win_Game" quando o jogador atingir o score 20
+        SceneManager.LoadScene("Win_Game");
+    }
+
+    public void InitGame()
+    {
+        // Carrega a cena de "Win_Game" quando o jogador atingir o score 20
+        SceneManager.LoadScene("Game");
+    }
+    public void InitMenu()
+    {
+        // Carrega a cena de "Win_Game" quando o jogador atingir o score 20
+        SceneManager.LoadScene("Menu");
+    }
+
+}
---
+++ Platform_Game/Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Animator playerAnim;
+    private Rigidbody2D rbPlayer;
+    public float speed;
+    private SpriteRenderer sr;
+    public float jumpforce;
+    private bool inFloor = true;
+
+    private GameControlle gcPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gcPlayer = GameControlle.gc;
+        gcPlayer.estrelas = 0;
+        playerAnim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+        rbPlayer = GetComponent<Rigidbody2D>();
+
+    }
+
+    void FixedUpdate(){
+        MovePlayer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Jump();
+    }
+
+    void MovePlayer()
+    {
+        float horizontalMoviment = Input.GetAxisRaw("Horizontal");
+        rbPlayer.linearVelocity = new Vector2(horizontalMoviment * speed, rbPlayer.linearVelocity.y);
+    
+        if (horizontalMoviment > 0){
+            playerAnim.SetBool("Walk", true);
+            sr.flipX = false;
+        }
+        else if (horizontalMoviment < 0){
+            playerAnim.SetBool("Walk", true);
+            sr.flipX = true;
+        }
+        else{
+            playerAnim.SetBool("Walk", false);
+        }
+    }
+
+    void Jump(){
+        if (Input.GetButtonDown("Jump") && inFloor){
+            playerAnim.SetBool("Jump", true);
+            rbPlayer.AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
+            inFloor = false;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Tilemap"){
+            playerAnim.SetBool("Jump", false);
+            inFloor = true;
+        }
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision){
+        if(collision.gameObject.tag == "Estrelas"){
+            Destroy(collision.gameObject);
+            gcPlayer.estrelas++;
+            gcPlayer.estrelaText.text = gcPlayer.estrelas.ToString();
+        }
+    }
+}
